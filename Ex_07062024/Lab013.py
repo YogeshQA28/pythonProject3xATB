0a5a6ac9f03c4888b217d4fab94dbc6acb85976f
Yogesh1092 <yogeshkumar.fnd@gmail.com>
2024-06-07 08:19:02 +0530

    for 7 june class

---
+++ Ex_07062024/Lab013.py
@@ -0,0 +1,16 @@
+#Advance data type in python
+# List,Tuple,Set,Dictionary
+
+
+# num1=input("Enter first number")
+# num2=input("Enter second number")
+# #result=num1+num2
+# result=int(num1)+int(num2)
+# print(result)
+
+
+#Formate of the string
+fname="Raj"
+Lname="Kumar"
+print("My name is {} {}".format(fname,Lname))
+print(f"My name is {fname} {Lname}")
